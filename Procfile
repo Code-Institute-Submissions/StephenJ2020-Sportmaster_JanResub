406f60f056580c53eea59ca65ce0f2f65367e9b5
Stephen <sjohnston.ie@gmail.com>
2021-12-08 14:56:40 +0000

    feat: Create Procfile add hostname and deploy to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sportmaster.wsgi.application
--- sportmaster/settings.py
+++ sportmaster/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sportmaster-irl.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -111,17 +111,17 @@ WSGI_APPLICATION = 'sportmaster.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-# commented out to switch to postgres DB below - may need to remove this code above before submission!!!!
-
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation

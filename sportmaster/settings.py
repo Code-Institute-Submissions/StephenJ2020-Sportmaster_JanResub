414d47eca90bed99ce3b765a6e696a0ea3b7d408
Stephen <sjohnston.ie@gmail.com>
2021-12-08 00:27:59 +0000

    fix: Hopefully resolve the Gitpod issue with requirements.txt this time

--- sportmaster/settings.py
+++ sportmaster/settings.py
@@ -109,19 +109,19 @@ WSGI_APPLICATION = 'sportmaster.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-# commented out to switch to postgres DB below - may need to remove this code above before submission!!!!
+# https://docs.djangoproject.com/en/3.2/ref/settings/databases
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+# commented out to switch to postgres DB below - may need to remove this code above before submission!!!!
+
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+# }
 
 
 # Password validation

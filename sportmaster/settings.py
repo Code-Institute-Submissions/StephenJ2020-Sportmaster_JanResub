929eb10895fc4b668de8326a870408ddef221673
Stephen <sjohnston.ie@gmail.com>
2021-12-21 18:44:16 +0000

    feat: Update webhook_handler.py to send confirmation email

--- sportmaster/settings.py
+++ sportmaster/settings.py
@@ -203,6 +203,7 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY")
 STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
 STRIPE_WH_SECRET = os.environ.get("STRIPE_WH_SECRET")
+DEFAULT_FROM_EMAIL = 'sportmaster-irl@example.com'
 
 if 'USE_AWS' in os.environ:
     # Cache control

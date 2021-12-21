dad181e543e14c2e52ae9db51239ca780bd431b8
Stephen <sjohnston.ie@gmail.com>
2021-12-21 23:18:17 +0000

    feat: Add product functionality completed

--- products/views.py
+++ products/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
 
-from .models import Product,Category
+from .models import Product, Category
 from .forms import ProductForm
 
 
@@ -42,7 +42,8 @@ def all_products(request):
                 messages.error(request, "You didn't enter any search criteria")
                 return redirect(reverse('products'))
             # read queries section of Django Doc regarding use of Q
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
             # i makes queries case insensitive
             products = products.filter(queries)
 
@@ -72,7 +73,18 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """ Add a product to the store """
-    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure \
+                           the form is valid.')
+    else:
+        form = ProductForm()
+
     template = 'products/add_product.html'
     context = {
         'form': form,

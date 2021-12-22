8d9c5442b4005ae38ce3cb556b96d92404735362
Stephen <sjohnston.ie@gmail.com>
2021-12-22 23:10:00 +0000

    feat: Set-up live email confirmations via sportmaster.ie@gmail.com

--- sportmaster/settings.py
+++ sportmaster/settings.py
@@ -116,7 +116,7 @@ if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'    # needs to be update before submission !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')

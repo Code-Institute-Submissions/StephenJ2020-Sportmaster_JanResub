efe53ac0fc0932d52221dc5d7c99a97683cebad0
Stephen <sjohnston.ie@gmail.com>
2021-12-23 03:22:35 +0000

    feat: Add review class to models.py in reviews app

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,8 +1,7 @@
-''' DocString '''
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='products'),
-    path('<int:product_id>/', views.product_review, name='product_review'),
+    path('', views.reviews, name='reviews'),
+    path('product_review/', views.product_review, name='product_review'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import Http404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from profiles.models import UserProfile
+from .models import Review
+from .forms import ProductReview
 
-# Create your views here.
+# Create your views here
+
+
+def reviews(request):
+    """ A view to return the review page """
+    reviews = Review.objects.order_by('-date_created')
+    context = {
+        'reviews': reviews
+    }
+    return render(request, 'reviews/reviews.html', context)
+
+
+@login_required
+def product_review(request):
+    """A view to allow users to write their own reviews
+    and add them to the site.
+    """
+    if request.method == 'POST':
+        form = ProductReview(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.creator = UserProfile.objects.get(user=request.user)
+            form.save()
+            messages.success(request, 'Review added!')
+            return redirect('reviews')
+        else:
+            messages.error(
+                request,
+                'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductReview()
+
+    template = 'reviews/product_review.html'
+    context = {
+        'form': form
+    }
+    return render(request, template, context)

e3d2e62330900e8edef8cd554280ca75a7a9242d
Stephen <sjohnston.ie@gmail.com>
2022-01-22 15:38:37 +0000

    feat: Update urls.py in reviews app

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
 
+# urlpatterns = [
+#     path('', views.reviews, name='reviews'),
+#     path('product_review/', views.product_review, name='product_review'),
+# ]
+
 urlpatterns = [
-    path('', views.reviews, name='reviews'),
-    path('product_review/', views.product_review, name='product_review'),
+    path('', views.reviews_total, name='reviews_total'),
+    path('add/', views.add_reviews, name='add_reviews'),
+    path('update/<int:review_id>/',
+         views.update_reviews, name='update_reviews'),
+    path('remove/<int:review_id>/', views.remove_review, name='remove_review'),
 ]

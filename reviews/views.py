852bada0354aa95ceb14765bff23dfa712577378
Stephen <sjohnston.ie@gmail.com>
2022-01-22 15:39:10 +0000

    feat: Update views.py in reviews app

--- reviews/views.py
+++ reviews/views.py
@@ -1,45 +1,116 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.http import Http404
-from django.contrib import messages
+""" Views for Reading, Creating, Updating and Removing Reviwes """
+
+from django.shortcuts import render, reverse, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from profiles.models import UserProfile
+from django.contrib import messages
+from django.db.models.functions import Lower
+
 from .models import Review
-from .forms import ProductReview
-
-# Create your views here
+from .forms import ReviewForm
 
 
-def reviews(request):
-    """ A view to return the review page """
-    reviews = Review.objects.order_by('-date_created')
+def reviews_total(request):
+    """ View for all reviews """
+
+    reviews = Review.objects.all()  # pylint(no-member)[15,15]
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sorting = request.GET['sort']
+            sort = sorting
+            if sorting == 'username':
+                reviews = reviews.annotate(
+                    lower_username=Lower('username__username'))
+            if sorting == 'watches':
+                sorting = 'watches__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sorting = f'-{sorting}'
+            reviews = reviews.order_by(sorting)
+
+    current_sorting = f'{sort}_{direction}'
+
+    template = 'reviews/reviews.html'
+
     context = {
-        'reviews': reviews
+        'reviews': reviews,
+        'current_sorting': current_sorting,
     }
-    return render(request, 'reviews/reviews.html', context)
+
+    return render(request, template, context)
 
 
 @login_required
-def product_review(request):
-    """A view to allow users to write their own reviews
-    and add them to the site.
-    """
-    if request.method == 'POST':
-        form = ProductReview(request.POST)
+def add_reviews(request):
+    """ Add reviews of the watches """
+
+    if request.method == 'POST' and request.user.is_authenticated:
+        form = ReviewForm(request.POST)
         if form.is_valid():
             review = form.save(commit=False)
-            review.creator = UserProfile.objects.get(user=request.user)
-            form.save()
-            messages.success(request, 'Review added!')
-            return redirect('reviews')
+            review.username = request.user
+            review.save()
+            messages.success(request, "Your review has" +
+                             "been submitted successfully")
         else:
-            messages.error(
-                request,
-                'Failed to add product. Please ensure the form is valid.')
+            messages.error(request, 'Sorry, we could not' +
+                           'submit you review at this time')
     else:
-        form = ProductReview()
+        form = ReviewForm()
 
-    template = 'reviews/product_review.html'
+    template = 'reviews/add_reviews.html'
     context = {
-        'form': form
+        'form': form,
     }
     return render(request, template, context)
+
+
+@login_required
+def update_reviews(request, review_id):
+    """ Update reviews of the watches """
+
+    if not request.user.is_superuser:
+        messages.error(request, "Sorry, only authorised users" +
+                       "can update reviews")
+        return redirect(reverse('home'))
+
+    review = get_object_or_404(Review, pk=review_id)
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            review = form.save()
+            messages.success(request, "Review updated successfully")
+            return redirect(reverse('update_reviews', args=[review.id]))
+        else:
+            messages.error(
+                request, "Update failed. Please ensure the form is valid.")
+            return redirect(reverse('reviews_total'))
+    else:
+        form = ReviewForm(instance=review)
+        messages.info(request, f'You are editing {review.watches}')
+
+    template = 'reviews/update_review.html'
+
+    context = {
+        'form': form,
+        'review': review,
+    }
+    return render(request, template, context)
+
+
+@login_required
+def remove_review(request, review_id):
+    """ Remove reviews of the watches """
+
+    review = get_object_or_404(Review, pk=review_id)
+    if request.user.is_superuser or review.username == request.user:
+        review.delete()
+        messages.success(request, "Review has been removed" +
+                         "successfuly")
+    else:
+        messages.error(request, 'Sorry, we could not' +
+                       'delete you review at this time')
+    return redirect(reverse('reviews_total'))

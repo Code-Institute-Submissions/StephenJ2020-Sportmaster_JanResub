aaba2107f3641aa09b8130649d565e9f64bf0932
Stephen <sjohnston.ie@gmail.com>
2022-01-28 01:26:50 +0000

    feat: Add classes to admin.py in announcements app

--- announcements/admin.py
+++ announcements/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Announcements, Post
 
-# Register your models here.
+
+class AnnouncementsAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+    )
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'blurb',
+        'image',
+        'date_added',
+        'author'
+    )
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Announcements, AnnouncementsAdmin)
